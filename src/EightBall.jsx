23fa1bdc0bd958b24eaf2b4bb697c60cd55bee54
danx <rushn24@gmail.com>
2024-07-05 09:11:03 -0400

    added very basic button component

---
+++ src/EightBall.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './EightBall.css'
+
+const EightBall = () => {
+    return (
+        <div className="EightBall">
+            <h1>Magic Eight Ball</h1>
+            <button className="EightBall-button">Think of a question</button>
+        </div>
+    )
+};
+
+export default EightBall;
